df28c83891357cd767a4f304ccd01d5b058dc60b
Francisco Güemes <francisco.gueemes@uniscon.net>
2020-03-05 18:12:48 +0100

    Setup for the Maven project.

---
+++ to-browser-plugin/bp-webapp/src/test/java/com/franciscoguemes/samples/bpwebapp/BpWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franciscoguemes.samples.bpwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
